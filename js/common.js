6dbee86699298195792f677575338350135dd9ab
bipeo <katamason1990@gmail.com>
2017-04-01 12:40:04 +0300

    Merge origin/master
    Conflicts:
    	travelp/app/css/style.css

---
+++ js/common.js
@@ -0,0 +1,313 @@
+$(function() {
+    
+
+	// styler
+	$('select').styler();
+	
+	// datepicker
+     
+	$( "#datepicker" ).datepicker({
+        	
+         beforeShow : function(){
+           if(!$('.datepicker_wrapper').length){
+                  $('#ui-datepicker-div').wrap('<span class="datepicker_wrapper"></span>');
+            }
+            $('.label_datepicker').addClass('cselect'); 
+        },        
+     
+         onClose: function (dateText, inst) {
+        var date = $(this).val();
+        $("#datepicker").val(date.toString());
+        $('.label_datepicker').removeClass('cselect');     
+        }
+	}).datepicker("setDate", new Date());
+	
+	// search
+	$('.js_search_button').on('click', function() {
+		$('.search_block').stop().slideToggle().toggleClass('show_search_block');
+	});
+	
+	$(document).mouseup(function (e) {
+        var container = $(".search_block, .search_btn");
+       
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+         $(".search_block").slideUp().removeClass('show_search_block');
+        }
+    });
+	
+	// slick
+	$('.js_slick_slider').slick({
+		arrows: true,
+		dots: false,
+		slidesToShow: 4,
+  		slidesToScroll: 4,
+		responsive: [
+			{
+				breakpoint: 1439,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3
+				}
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 670,
+				settings: {
+					arrows: false,
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 640,
+				settings: {
+					arrows: false,
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					verticalSwiping: false,
+					vertical: true
+					
+				}
+			}
+		]
+	});
+	
+	//#start сообщения внизу страницы
+        
+        $('.message_send_block').each(function() {
+            var elem = $(this).attr('href');
+            var mh = $(this).outerHeight();
+            $(this).css({'bottom': '-'+mh+'px'} );
+   
+        })
+	
+	$('.js_message').on('click',function(){
+		$('.js_message').css('display', 'block');
+		$(this).css('display', 'none');
+                var elem = $(this).attr('href');
+                var mesb = $('#message_send_block').outerHeight();
+                var sb = $('#subscribe_block').outerHeight();
+                if(elem == '#subscribe_block') {
+       		$('#message_send_block').animate({bottom:'-'+mesb+'px'} , 500);
+                }
+                if(elem == '#message_send_block') {
+       		$('#subscribe_block').animate({bottom:'-'+sb+'px'} , 500);
+                }
+		$(elem).animate({bottom:'0'},500);
+		return false;
+	});
+	
+	$('.message_close').on('click', function(){
+		$('.js_message').css('display', 'block');
+                var hpar =  $(this).parent().outerHeight(); 
+		$(this).parent().animate({bottom:'-'+hpar+'px'},300);
+	});
+        
+        
+        
+          $(document).mouseup(function (e)
+    {
+        var container = $('.message_send_block, .fixed_btn ');
+
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+            
+            $('.message_send_block').each(function() {
+            var mh = $(this).outerHeight();
+            $(this).animate({'bottom': '-'+mh+'px'} ,{
+                complete:function() {
+                $('.fixed_btn a').show()    
+                }
+            }, 300 );
+           
+   
+        })
+           
+        }
+    });
+    
+    //end #########сообщения внизу страницы
+	
+	// кнопка фильтра
+	
+	$('.show_hide_filtr').click(function() {
+		$('.gray_block').slideToggle(function() {
+			if($(this).is(":hidden")) {
+				$('.show_hide_filtr').text('Показать фильтр');
+			}
+			if($(this).is(":visible")) {
+				$('.show_hide_filtr').text('Скрыть фильтр');
+			}
+		});
+		
+	});
+        
+            $(document).mouseup(function (e)
+    {
+        var container = $(".gray_block, .show_hide_filtr, .mega_menu > li, .datepicker_wrapper ");
+
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+            
+            if($('.show_hide_filtr').is(':visible')) {
+                $('.gray_block').slideUp();
+            $('.show_hide_filtr').text('Показать фильтр'); 
+                    }
+           
+           
+        }
+    });
+	
+	
+	// высота блока
+	
+
+        
+        /*****start*** */
+         $('.link_1').on('click',function() {
+              $('.auth_reg_block').slideDown();
+              var h_block_2 = $('.auth_reg_block').innerHeight();
+              console.log(h_block_2);
+          })
+          
+          $('.auth_reg_block .btn_1.show_640').on('click',function() {
+              $(".auth_reg_block").slideUp()
+          })
+              $(document).mouseup(function (e)
+    {
+        var container = $(".auth_reg_block");
+
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+            container.slideUp();
+        }
+    });
+    /****end**** */
+
+	
+        
+	setTimeout(function() {
+		$('.gray_select_style.width_4.jq-selectbox').change(function() {
+			var zz = $(this).find('.jq-selectbox__select-text').text();
+			$(this).find('.jq-selectbox__select').attr("id", zz);
+			$('.gray_select_style.width_4 .jq-selectbox__select-text').text('Цвет')
+		});     
+	},100);
+        
+        $('.gray_select_style.width_4 .jq-selectbox__select-text').text('Цвет')
+        $('.gray_select_style.width_4 ul li').each(function(i) {
+		$(this).attr('class', 'inc_' + i )    
+         	console.log(i)
+		});   
+       
+       
+       
+       
+       //.mega_menu  
+       var dtclick = function() {
+           
+           
+       
+       if ($('.mega_menu  .tab-content').css('z-index') == '2') {
+           
+       
+       	$('.mega_menu > li > a').unbind('click').bind('click',function(){
+               
+		var tab_id = $(this).parent().attr('data-tab');
+               
+
+		$('.mega_menu > li').not($(this).parent()).removeClass('current');
+		$('.mega_menu > li > ul').not($("#"+tab_id)).removeClass('current');
+
+		$(this).parent().toggleClass('current');
+		$("#"+tab_id).toggleClass('current');
+                
+                return false;
+	})
+        
+        
+            	$('.mega_menu > li > ul .h3').unbind('click').bind('click',function(){
+               
+		var tbt_id = $(this).attr('data-tbt');
+
+		$('.mega_menu__column ul').not($("#"+tbt_id)).removeClass('tbtcur');
+		$('.mega_menu > li > ul .h3').not($(this)).removeClass('tbtcur');
+
+		$("#"+tbt_id).toggleClass('tbtcur');
+                $(this).toggleClass('tbtcur');  
+                
+                return false;
+                })
+                } 
+                
+          }
+          
+          dtclick();
+          $(window).on('resize',function() {
+		dtclick();
+	}) ;
+        
+        
+        
+        var tzC =  function() {
+            if($('.menu_mobile_phone').css('z-index') == '1') {
+            $('.phone_number').click(function() {
+                
+             // $('.menu_mobile_phone').toggleClass('vibible');
+             $('.menu_mobile_phone').stop().slideToggle()
+              
+            })
+        }
+            }
+         tzC();
+        
+         
+         
+          $(document).mouseup(function (e)
+    {
+        var container = $(".menu_mobile_phone, .phone_number");
+
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+            container.removeClass('vibible');
+            $('.menu_mobile_phone').slideUp()
+        }
+    });
+    
+    
+          $(document).mouseup(function (e)
+    {
+        var container = $(".mega_menu > li");
+
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+            container.removeClass('current');
+            $('.mega_menu  .tab-content').removeClass('current');
+        }
+    });
+    
+    
+     // mask
+    $('.for_mask').mask('+7 (000) 000-00-00');
+         
+        
+        
+ 
+
+        
+               
+
+});
